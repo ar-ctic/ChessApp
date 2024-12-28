f2aa282025e39bb7062e7ee87640a9992b21a721
ar-ctic <daze3456@gmail.com>
2024-12-28 17:23:27 +0100

    Adds sending JSON to ws

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,11 +1,14 @@
 package utils
 
 import (
+	"ChessApp/types"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"log"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/websocket"
 )
 
 var Validate = validator.New()
@@ -29,3 +32,22 @@ func WriteError(w http.ResponseWriter, status int, err error) {
 	WriteJSON(w, status, map[string]string{"error": err.Error()})
 
 }
+
+func SendJSON(conn *websocket.Conn, success bool, status int, message string) {
+	errorMessage := types.ErrorMessage{
+		Success:   success,
+		Message: message,
+		Status:  status,
+	}
+	jsonData, err := json.Marshal(errorMessage)
+	if err != nil {
+		log.Println("Error marshalling error message:", err)
+		return
+	}
+	fmt.Printf("Data to send: %s\n", string(jsonData))
+	err = conn.WriteMessage(websocket.TextMessage, jsonData)
+	if err != nil {
+		log.Println("Error sending WebSocket message:", err)
+	}
+}
+
